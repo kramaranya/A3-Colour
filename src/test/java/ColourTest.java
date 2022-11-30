f2e011826e210ebe1e54b98c0af08b62b56dcb20
kramaranya <AKramarIK02@office365.fiot.kpi.ua>
2022-11-30 19:03:51 +0000

    Create a test to check the correctness of creating a colour from three valid components

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -1,7 +1,15 @@
 import colour.Colour;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
-import static org.junit.Assert.assertEquals;
 
 public class ColourTest {
+    @Test
+    void Colour_ThreeValidComponents_CreateColourWithCorrectValues() {
+        Colour colour = new Colour(0.0F, 0.5F, 1.0F);
+
+        Assertions.assertEquals(0, colour.getRed());
+        Assertions.assertEquals(255 / 2, colour.getGreen());
+        Assertions.assertEquals(255, colour.getBlue());
+    }
 }
