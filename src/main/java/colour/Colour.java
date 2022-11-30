337f9c9db66684f1e86401b054df6a2f980ff949
kramaranya <AKramarIK02@office365.fiot.kpi.ua>
2022-11-30 19:11:00 +0000

    Create a constructor with three float inputs,three fields and getters for red, green and blue components to pass the test

--- src/main/java/colour/Colour.java
+++ src/main/java/colour/Colour.java
@@ -3,9 +3,30 @@ package colour;
 /**
  * The {@code Colour} class is used to represent RGB colours.
  *
- * @version 29 Nov 2022
  * @author Anna Kramar
+ * @version 29 Nov 2022
  */
 public class Colour {
+    private final int red;
+    private final int green;
+    private final int blue;
 
+    public Colour(float r, float g, float b) {
+
+        red = (int) (r * 255);
+        green = (int) (g * 255);
+        blue = (int) (b * 255);
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
 }
