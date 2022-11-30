ea63d6ec3f7aca3c8be92c5501b58109df700d5e
kramaranya <AKramarIK02@office365.fiot.kpi.ua>
2022-11-30 17:36:28 +0000

    Add Junit dependencies in pom.xml, create Colour.java class and ColourTest.java class for testing

---
+++ src/main/java/colour/Colour.java
@@ -0,0 +1,11 @@
+package colour;
+
+/**
+ * The {@code Colour} class is used to represent RGB colours.
+ *
+ * @version 29 Nov 2022
+ * @author Anna Kramar
+ */
+public class Colour {
+
+}
---
+++ src/test/java/ColourTest.java
@@ -0,0 +1,7 @@
+import colour.Colour;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ColourTest {
+}
