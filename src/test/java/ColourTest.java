b7f9794592357ae8d1442991ccf94b4439e68772
kramaranya <AKramarIK02@office365.fiot.kpi.ua>
2022-11-30 20:45:14 +0000

    Add few tests to check the correctness of creating colours

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -7,25 +7,66 @@ public class ColourTest {
     @Test
     void Colour_ThreeValidComponents_CreateColourWithCorrectValues() {
         Colour colour = new Colour(0.0F, 0.5F, 1.0F);
+        Colour colour2 = new Colour(0.0F, 0.0F, 0.0F);
+        Colour colour3 = new Colour(1.0F, 1.0F, 1.0F);
 
         Assertions.assertEquals(0, colour.getRed());
         Assertions.assertEquals(255 / 2, colour.getGreen());
         Assertions.assertEquals(255, colour.getBlue());
+
+        Assertions.assertEquals(0, colour2.getRed());
+        Assertions.assertEquals(0, colour2.getGreen());
+        Assertions.assertEquals(0, colour2.getBlue());
+
+        Assertions.assertEquals(255, colour3.getRed());
+        Assertions.assertEquals(255, colour3.getGreen());
+        Assertions.assertEquals(255, colour3.getBlue());
     }
+
+    @Test
+    void Colour_ThreeValidComponents_CreateColoursWithCorrectValues() {
+        Colour colour = new Colour(0.0F, 0.0F, 0.0F);
+        Colour colour2 = new Colour(1.0F, 1.0F, 1.0F);
+
+        Assertions.assertEquals(0, colour.getRed());
+        Assertions.assertEquals(0, colour.getGreen());
+        Assertions.assertEquals(0, colour.getBlue());
+
+        Assertions.assertEquals(255, colour2.getRed());
+        Assertions.assertEquals(255, colour2.getGreen());
+        Assertions.assertEquals(255, colour2.getBlue());
+    }
+
     @Test
     void Colour_OneComponentIsGreaterThanExpected_ThrowIllegalArgumentException() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Colour(0.0F, 0.5F, 15.0F));
     }
+
     @Test
     void Colour_OneComponentIsSmallerThanExpected_ThrowIllegalArgumentException() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Colour(0.0F, 0.5F, -1.0F));
     }
+
     @Test
-    void Colour_OneValidRGBComponent_CreateColourWithCorrectValues(){
+    void Colour_OneValidRGBComponent_CreateColourWithCorrectValues() {
         Colour colour = new Colour(16711935);
 
         Assertions.assertEquals(255, colour.getRed());
         Assertions.assertEquals(0, colour.getGreen());
         Assertions.assertEquals(255, colour.getBlue());
+    }
+
+    @Test
+    void Colour_ValidRGBComponents_CreateColoursWithCorrectValues() {
+        Colour colour = new Colour(0);
+        Colour colour2 = new Colour(16777215);
+
+        Assertions.assertEquals(0, colour.getRed());
+        Assertions.assertEquals(0, colour.getGreen());
+        Assertions.assertEquals(0, colour.getBlue());
+
+        Assertions.assertEquals(255, colour2.getRed());
+        Assertions.assertEquals(255, colour2.getGreen());
+        Assertions.assertEquals(255, colour2.getBlue());
     }
 }
