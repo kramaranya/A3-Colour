40dd9a6c9214654a9f87c2b20a74b33316c58b46
kramaranya <AKramarIK02@office365.fiot.kpi.ua>
2022-12-03 23:46:30 +0000

    Refactor: change unit test using AAA pattern

--- src/test/java/colour/ColourTest.java
+++ src/test/java/colour/ColourTest.java
@@ -8,96 +8,136 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ColourTest {
     @Test
     void Colour_ThreeValidComponents_CreateColourWithCorrectValues() {
+        int expectedRed = 0;
+        int expectedGreen = 127;
+        int expectedBlue = 255;
+
         Colour colour = new Colour(0.0F, 0.5F, 1.0F);
 
         Assertions.assertAll(
-                () -> Assertions.assertEquals(0, colour.getRed()),
-                () -> Assertions.assertEquals(255 / 2, colour.getGreen()),
-                () -> Assertions.assertEquals(255, colour.getBlue())
+                () -> Assertions.assertEquals(expectedRed, colour.getRed()),
+                () -> Assertions.assertEquals(expectedGreen, colour.getGreen()),
+                () -> Assertions.assertEquals(expectedBlue, colour.getBlue())
         );
     }
 
     @Test
     void Colour_ThreeValidComponents_CreateColoursWithCorrectValues() {
+        int expectedRed = 0;
+        int expectedGreen = 0;
+        int expectedBlue = 0;
+
         Colour colour = new Colour(0.0F, 0.0F, 0.0F);
 
         Assertions.assertAll(
-                () -> Assertions.assertEquals(0, colour.getRed()),
-                () -> Assertions.assertEquals(0, colour.getGreen()),
-                () -> Assertions.assertEquals(0, colour.getBlue())
+                () -> Assertions.assertEquals(expectedRed, colour.getRed()),
+                () -> Assertions.assertEquals(expectedGreen, colour.getGreen()),
+                () -> Assertions.assertEquals(expectedBlue, colour.getBlue())
         );
     }
 
     @Test
     void Colour_AnotherThreeValidComponents_CreateColoursWithCorrectValues() {
+        int expectedRed = 255;
+        int expectedGreen = 255;
+        int expectedBlue = 255;
+
         Colour colour = new Colour(1.0F, 1.0F, 1.0F);
 
         Assertions.assertAll(
-                () -> Assertions.assertEquals(255, colour.getRed()),
-                () -> Assertions.assertEquals(255, colour.getGreen()),
-                () -> Assertions.assertEquals(255, colour.getBlue())
+                () -> Assertions.assertEquals(expectedRed, colour.getRed()),
+                () -> Assertions.assertEquals(expectedGreen, colour.getGreen()),
+                () -> Assertions.assertEquals(expectedBlue, colour.getBlue())
         );
     }
 
     @Test
     void Colour_OneComponentIsGreaterThanExpected_ThrowIllegalArgumentException() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Colour(1.5F, 0.5F, 15.0F));
-        Assertions.assertEquals("Color parameter(s) out of range [0.0, 1.0]: red = 1.5 blue = 15.0", exception.getMessage());
+        String expectedMessage = "Color parameter(s) out of range [0.0, 1.0]: red = 1.5 blue = 15.0";
+
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> new Colour(1.5F, 0.5F, 15.0F));
+
+        Assertions.assertEquals(expectedMessage, exception.getMessage());
     }
 
     @Test
     void Colour_OneComponentIsSmallerThanExpected_ThrowIllegalArgumentException() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Colour(0.0F, 0.5F, -1.0F));
-        Assertions.assertEquals("Color parameter(s) out of range [0.0, 1.0]: blue = -1.0", exception.getMessage());
+        String expectedMessage = "Color parameter(s) out of range [0.0, 1.0]: blue = -1.0";
+
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> new Colour(0.0F, 0.5F, -1.0F));
+
+        Assertions.assertEquals(expectedMessage, exception.getMessage());
     }
 
     @Test
     void Colour_OneValidRGBComponent_CreateColourWithCorrectValues() {
+        int expectedRed = 255;
+        int expectedGreen = 0;
+        int expectedBlue = 255;
+
         Colour colour = new Colour(16711935);
 
         Assertions.assertAll(
-                () -> Assertions.assertEquals(255, colour.getRed()),
-                () -> Assertions.assertEquals(0, colour.getGreen()),
-                () -> Assertions.assertEquals(255, colour.getBlue())
+                () -> Assertions.assertEquals(expectedRed, colour.getRed()),
+                () -> Assertions.assertEquals(expectedGreen, colour.getGreen()),
+                () -> Assertions.assertEquals(expectedBlue, colour.getBlue())
         );
     }
 
     @Test
     void Colour_ValidRGBComponents_CreateColoursWithCorrectValues() {
+        int expectedRed = 170;
+        int expectedGreen = 170;
+        int expectedBlue = 170;
+
         Colour colour = new Colour(11184810);
 
         Assertions.assertAll(
-                () -> Assertions.assertEquals(170, colour.getRed()),
-                () -> Assertions.assertEquals(170, colour.getGreen()),
-                () -> Assertions.assertEquals(170, colour.getBlue())
+                () -> Assertions.assertEquals(expectedRed, colour.getRed()),
+                () -> Assertions.assertEquals(expectedGreen, colour.getGreen()),
+                () -> Assertions.assertEquals(expectedBlue, colour.getBlue())
         );
     }
 
     @Test
     void Colour_AnotherValidRGBComponents_CreateColoursWithCorrectValues() {
+        int expectedRed = 255;
+        int expectedGreen = 255;
+        int expectedBlue = 255;
+
         Colour colour = new Colour(16777215);
 
         Assertions.assertAll(
-                () -> Assertions.assertEquals(255, colour.getRed()),
-                () -> Assertions.assertEquals(255, colour.getGreen()),
-                () -> Assertions.assertEquals(255, colour.getBlue())
+                () -> Assertions.assertEquals(expectedRed, colour.getRed()),
+                () -> Assertions.assertEquals(expectedGreen, colour.getGreen()),
+                () -> Assertions.assertEquals(expectedBlue, colour.getBlue())
         );
     }
 
     @Test
     void Colour_ComponentIsGreaterThanExpected_ThrowIllegalArgumentException() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Colour(16777216));
-        Assertions.assertEquals("RGB parameter out of range [0, 16777215]: RGB = 16777216", exception.getMessage());
+        String expectedMessage = "RGB parameter out of range [0, 16777215]: RGB = 16777216";
+
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> new Colour(16777216));
+
+        Assertions.assertEquals(expectedMessage, exception.getMessage());
     }
 
     @Test
     void Colour_ComponentIsSmallerThanExpected_ThrowIllegalArgumentException() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Colour(-50));
-        Assertions.assertEquals("RGB parameter out of range [0, 16777215]: RGB = -50", exception.getMessage());
+        String expectedMessage = "RGB parameter out of range [0, 16777215]: RGB = -50";
+
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> new Colour(-50));
+
+        Assertions.assertEquals(expectedMessage, exception.getMessage());
     }
 
     @Test
-    void Equals_ColoursHaveTheSameComponentValues_ReturnTrue(){
+    void Equals_ColoursHaveTheSameComponentValues_ReturnTrue() {
         Colour colour = new Colour(1.0F, 0.0F, 1.0F);
         Colour colour2 = new Colour(16711935);
 
@@ -105,7 +145,7 @@ public class ColourTest {
     }
 
     @Test
-    void Equals_ColoursHaveNotTheSameComponentValues_ReturnFalse(){
+    void Equals_ColoursHaveNotTheSameComponentValues_ReturnFalse() {
         Colour colour = new Colour(1.0F, 1.0F, 1.0F);
         Colour colour2 = new Colour(0);
 
@@ -113,7 +153,7 @@ public class ColourTest {
     }
 
     @Test
-    void HashCode_ColoursHaveTheSameComponentValues_ReturnTrue(){
+    void HashCode_ColoursHaveTheSameComponentValues_ReturnTrue() {
         Colour colour = new Colour(1.0F, 0.0F, 1.0F);
         Colour colour2 = new Colour(16711935);
 
@@ -121,7 +161,7 @@ public class ColourTest {
     }
 
     @Test
-    void HashCode_ColoursHaveNotTheSameComponentValues_ReturnFalse(){
+    void HashCode_ColoursHaveNotTheSameComponentValues_ReturnFalse() {
         Colour colour = new Colour(1.0F, 0.0F, 1.0F);
         Colour colour2 = new Colour(0);
 
